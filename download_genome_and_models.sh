64c31c9d6bfc2c7aa63bf2ca1d50ef72673361ee
Kai-Li <kie8409@gmail.com>
2022-07-21 15:19:03 +0800

    change genome url

--- download_genome_and_models.sh
+++ download_genome_and_models.sh
@@ -4,7 +4,7 @@ hg38="https://osf.io/download/wzehu/?view_only=b72484b658874f9383cf25d209bc4000"
 pretrained_DNABERT="https://osf.io/download/9sjhd/?view_only=b72484b658874f9383cf25d209bc4000"
 fine_tuned_DNABERT="https://osf.io/download/dm48t/?view_only=b72484b658874f9383cf25d209bc4000"
 
-wget $genome_url -O hg38.zip
+wget $hg38 -O hg38.zip
 unzip hg38.zip
 mv -f human ./DNABERT/eccdna/genome/
 rm hg38.zip
